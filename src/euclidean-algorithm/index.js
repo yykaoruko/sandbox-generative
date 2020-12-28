22be666e84893eb5049cec61af6af77ee2f6ffd2
Kaoruko <yykaoruko@gmail.com>
2020-12-29 00:05:56 +0900

    長方形の正方形による分割

--- src/euclidean-algorithm/index.js
+++ src/euclidean-algorithm/index.js
@@ -6,6 +6,7 @@ function draw() {
   const numA = 10;
   const numB = 6;
   const scaler = 50;
+  const ratio = numB / numA;
 
   const numAA = numA * scaler;
   const numBB = numB * scaler;
@@ -13,22 +14,22 @@ function draw() {
   let xPosition = 0;
   let yPosition = 0;
   let count = 0;
-  let rectWidth = numBB;
+  let rectWidth = width;
   
-  while(rectWidth > 0) {
+  while(rectWidth > 0.1) {
     count++;
     if (count % 2 === 1) {
-      while(xPosition + rectWidth <= numAA) {
-        rect(xPosition, yPosition, rectWidth, rectWidth);
-        xPosition += rectWidth;
+      while(xPosition + rectWidth * ratio < width + 0.1) {
+        rect(xPosition, yPosition, rectWidth * ratio, rectWidth);
+        xPosition += rectWidth * ratio;
       }
       rectWidth = numAA - xPosition;
     } else {
-      while(yPosition + rectWidth <= numBB) {
-        rect(xPosition, yPosition, rectWidth, rectWidth);
-        yPosition += rectWidth;
+      while(yPosition + rectWidth / ratio < width + 0.1) {
+        rect(xPosition, yPosition, rectWidth, rectWidth / ratio);
+        yPosition += rectWidth / ratio;
       }
-      rectWidth = numBB - yPosition;
+      rectWidth = width - yPosition;
     }
   }
 }
